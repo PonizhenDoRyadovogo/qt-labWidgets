b1e716dff5546e48fb72f12cc80d715b39516612
PonizhenDoRyadovogo <art25ivan@gmail.com>
2025-02-18 14:04:14 +0700

    Refactor: Win now inherits from QWidget + added slots + replaced #pragma once with #ifndef... + added new class
    1. The 'Win' class now inherits from 'QWidget', as QWidget provides more customization for the window
    2. Added declaration for all necessary slots
    3. Added class member variables
    4. Removed '#pragma once' and replaced it with '#ifndef' include guards for better portability
    5. Introduced a new `StrValidator` class for input validation

--- program_squareNumber/Win.h
+++ program_squareNumber/Win.h
@@ -1,19 +1,46 @@
-#pragma once
+#ifndef WIN_H
+#define WIN_H
 
-#include <QMainWindow>
+#include <QWidget>
+#include <QFrame>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QValidator>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Win; }
 QT_END_NAMESPACE
 
-class Win : public QMainWindow
+class Win : public QWidget
 {
     Q_OBJECT
 
 public:
     Win(QWidget *parent = nullptr);
-    ~Win();
-
-private:
-    Ui::Win *ui;
+public slots:
+    void begin();
+    void calc();
+protected:
+    QFrame* frame;
+    QLabel* inputLabel;
+    QLineEdit* inputEdit;
+    QLabel* outputLabel;
+    QLineEdit* outputEdit;
+    QPushButton* nextButton;
+    QPushButton* exitButton;
 };
+
+class StrValidator: public QValidator
+{
+    StrValidator(QObject* parent = 0)
+        :QValidator(parent)
+    {}
+    // TODO: Implement the string processing method
+    virtual State validate(QString &str, int& pos) const
+    {
+        return Acceptable;
+    }
+};
+
+#endif //WIN_N
