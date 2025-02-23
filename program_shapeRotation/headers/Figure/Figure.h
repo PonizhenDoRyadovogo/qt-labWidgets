f63333d70509404d1a22104099b24dadf139b259
PonizhenDoRyadovogo <art25ivan@gmail.com>
2025-02-23 21:56:35 +0700

    Define Figure class and its subclasses MyLine and MyRect; Override move() in MyRect and MyLine
    - Defined the base class Figure with necessary attributes and methods
    - Implemented the subclasses MyLine and MyRect, inheriting from Figure
    - Overrode the virtual function move() in MyLine and MyRect to provide specific movement logic

--- program_shapeRotation/headers/Figure/Figure.h
+++ program_shapeRotation/headers/Figure/Figure.h
@@ -1,11 +1,38 @@
 #ifndef FIGURE_H
 #define FIGURE_H
 
+#include <QPainter>
 
 class Figure
 {
 public:
-    Figure();
+    Figure(int X, int Y, int Halflen)
+        :x(X), y(Y), halflen(Halflen)
+    {}
+    void move(float Alpha, QPainter *painter);
+protected:
+    int x, y, halflen, dx, dy, r;
+    virtual void draw(QPainter *painter) = 0;
+};
+
+class MyLine: public Figure
+{
+public:
+    MyLine(int x, int y, int halflen)
+        :Figure(x, y, halflen)
+    {}
+protected:
+    void draw(QPainter *painer) override;
+};
+
+class MyRect: public Figure
+{
+public:
+    MyRect(int x, int y, int halflen)
+        :Figure(x, y, halflen)
+    {}
+protected:
+    void draw(QPainter *painter) override;
 };
 
 #endif // FIGURE_H
--- program_shapeRotation/src/Figure/Figure.cpp
+++ program_shapeRotation/src/Figure/Figure.cpp
@@ -1,6 +1,23 @@
+#include <math.h>
+
 #include "Figure.h"
 
-Figure::Figure()
+void Figure::move(float alpha, QPainter *painter)
 {
+    dx = halflen * cos(alpha);
+    dy = halflen * sin(alpha);
+    draw(painter);
+}
 
+void MyLine::draw(QPainter *painter)
+{
+    painter->drawLine(x + dx, y + dy, x - dx, y - dy);
+}
+
+void MyRect::draw(QPainter *painter)
+{
+    painter->drawLine(x + dx, y + dy, x + dy, y - dx);
+    painter->drawLine(x + dy, y - dx, x - dx, y - dy);
+    painter->drawLine(x - dx, y - dy, x - dy, y + dx);
+    painter->drawLine(x - dy, y + dx, x + dx, y + dy);
 }
